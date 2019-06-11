d543dac3347e2268b767f0c699e12b23e0da7f81
Egbert Schroeder <egbertschroeder@gmail.com>
2019-06-11 14:45:36 +0200

    Update custom_viz_text.js

--- custom_viz_text.js
+++ custom_viz_text.js
@@ -1,86 +1,83 @@
 /**
- * Welcome to the Looker Visualization Builder! Please refer to the following resources 
- * to help you write your visualization:
- *  - API Documentation - https://github.com/looker/custom_visualizations_v2/blob/master/docs/api_reference.md
- *  - Example Visualizations - https://github.com/looker/custom_visualizations_v2/tree/master/src/examples
+ * This is a Custom Visualization for Looker
+ *
+ * It shows the first 2 dimension columns from the fir result as a title and a paragraph
+ *
+ * Created by: Egbert Schroeder
  **/
 
-const customVizText = {
+const customVisTextFromData = {
   /**
-   * Configuration options for your visualization. In Looker, these show up in the vis editor
-   * panel but here, you can just manually set your default values in the code.
+   * Configuration options
+   *
    **/
-  options: {
-     font_size: {
-       type: "string",
-       label: "Font Size",
-       values: [
-         {"Large": "large"},
-         {"Small": "small"}
-       ],
-       display: "radio",
-       default: "small"
-     }
-   },
-  
+
+  options: {},
+
   /**
    * The create function gets called when the visualization is mounted but before any
    * data is passed to it.
    **/
-     create: function(element, config){
-          // Insert a <style> tag with some styles we'll use later.
-     var css = element.innerHTML = `
-     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
-     `;
- 
-     // Create a container element to let us center the text.
-     var container = element.appendChild(document.createElement("div"));
-    
- 
-     // Create an element to contain the text.
-    this._textElement = container.appendChild(document.createElement("div"));
-    this._textElement.className = "container-fluid";
-    },
- 
+  create: function(element, config) {
+    // Insert Bootstrap css file
+    element.innerHTML =
+      '<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">';
+
+    // Create a container element to let us center the text.
+    this._visContainer = element.appendChild(document.createElement("div"));
+    this._visContainer.className = "container-fluid";
+  },
+
   /**
    * UpdateAsync is the function that gets called (potentially) multiple times. It receives
    * the data and should update the visualization with the new data.
    **/
-     updateAsync: function(data, element, config, queryResponse, details, doneRendering){
-        // Clear any errors from previous updates
-     this.clearErrors();
- 
-     // Throw some errors and exit if the shape of the data isn't what this chart needs
-     if (queryResponse.fields.dimensions.length == 0) {
-       this.addError({title: "No Dimensions", message: "This chart requires dimensions."});
-       return;
-     }
- 
-     // Grab the first cell of the data
-    var html_start = "<dl class=\"row\">";
-    // <dt class="col-sm-3">Description lists</dt>
-    // <dd class="col-sm-9">A description list is perfect for defining terms.</dd>
-    html = "";
-  for(var row of data) {
-    var cell1 = row[queryResponse.fields.dimensions[0].name];
-    html += '<dt class="col-sm-12">'+LookerCharts.Utils.htmlForCell(cell1)+'</dt>';
+  updateAsync: function(
+    data,
+    element,
+    config,
+    queryResponse,
+    details,
+    doneRendering
+  ) {
+    // Clear any errors from previous updates
+    this.clearErrors();
 
-    var cell2 = row[queryResponse.fields.dimensions[1].name];
-    html += '<dd class="col-sm-12">'+LookerCharts.Utils.htmlForCell(cell2)+'</dd>';
-  }
-  var html_end = "<dl>";
-     // Insert the data into the page
-    this._textElement.innerHTML = html_start + html + html_end;
- 
-     // Set the size to the user-selected size
-     if (config.font_size == "large") {
-      
-     } else {
-      
-     }
+    // Throw some errors and exit if the shape of the data isn't what this chart needs
+    if (queryResponse.fields.dimensions.length == 0) {
+      this.addError({
+        title: "No Dimensions",
+        message: "This chart requires dimensions."
+      });
+      return;
+    }
+
+    // Grab the first cell of the data
+    var first_row = data[0];
+
+    // Grab first column for the title
+    var title = first_row[queryResponse.fields.dimensions[0].name];
+    
+    // Grab second column for the paragraph
+    var paragraph = first_row[queryResponse.fields.dimensions[1].name];
+
+    // Insert the data into text elements 
+    var html = '<div class="row">';
+    html +=
+      '<h4 class="col-sm-12">' +
+      LookerCharts.Utils.htmlForCell(title) +
+      "</h4>";
+    html +=
+      '<p class="col-sm-12">' +
+      LookerCharts.Utils.htmlForCell(paragraph) +
+      "</p>";
+    html += "</div>";
+    
+    // Insert the generated html into the page
+    this._visContainer.innerHTML = html;
+
     doneRendering();
-     }
+  }
+};
 
- };
- 
- looker.plugins.visualizations.add(customVizText);
+looker.plugins.visualizations.add(customVisTextFromData);
