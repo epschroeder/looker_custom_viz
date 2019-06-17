895345736fada41664c46b777f710cc225e98b49
Egbert Schroeder <egbertschroeder@gmail.com>
2019-06-17 16:08:42 +0200

    Added option to show row numbers

--- custom_viz_datatable.js
+++ custom_viz_datatable.js
@@ -15,12 +15,12 @@
  * TODO:
  *
  * [ ] Add Table Calculations
- * [ ] Conditional Formatting
- * [ ] Sort order for multpiple columns
- * [ ] Pivot
- * [ ] Totals
- * [ ] Row Totals
- * [ ] Row Numbers
+ * [ ] Add Conditional Formatting
+ * [ ] Add Sort for multpiple columns
+ * [ ] Add Pivot Fields
+ * [ ] Add Totals
+ * [ ] Add Row Totals
+ * [o] Row Numbers
  * [ ] Add Option: Series name override
  * [ ] Add Option: Font Size
  * [ ] Add Option: Cell Padding
@@ -38,154 +38,154 @@ const customVizDataTable = {
 
     options: {
         // FORMATTING
-    enableConditionalFormatting: {
-      default: false,
-      label: "Enable Conditional Formatting",
-      order: 1,
-      section: "Formatting",
-      type: "boolean"
+        enableConditionalFormatting: {
+            default: false,
+            label: "Enable Conditional Formatting",
+            order: 1,
+            section: "Formatting",
+            type: "boolean"
+        },
+        perColumnRange: {
+            default: true,
+            hidden: true,
+            label: "Per column range",
+            order: 2,
+            section: "Formatting",
+            type: "boolean"
+        },
+        conditionalFormattingType: {
+            default: "all",
+            display: "select",
+            label: "Formatting Type",
+            order: 3,
+            section: "Formatting",
+            type: "string",
+            values: [
+                {All: "all"},
+                {"Subtotals only": "subtotals_only"},
+                {"Non-subtotals only": "non_subtotals_only"}
+            ]
+        },
+        includeNullValuesAsZero: {
+            default: false,
+            label: "Include Null Values as Zero",
+            order: 4,
+            section: "Formatting",
+            type: "boolean"
+        },
+        formattingStyle: {
+            default: "low_to_high",
+            display: "select",
+            label: "Format",
+            order: 5,
+            section: "Formatting",
+            type: "string",
+            values: [
+                {"From low to high": "low_to_high"},
+                {"From high to low": "high_to_low"}
+            ]
+        },
+        formattingPalette: {
+            default: "red_yellow_green",
+            display: "select",
+            label: "Palette",
+            order: 6,
+            section: "Formatting",
+            type: "string",
+            values: [
+                {"Red to Yellow to Green": "red_yellow_green"},
+                {"Red to White to Green": "red_white_green"},
+                {"Red to White": "red_white"},
+                {"White to Green": "white_green"},
+                {"Custom...": "custom"}
+            ]
+        },
+        lowColor: {
+            display: "color",
+            display_size: "third",
+            label: "Low", // These values updated in updateAsync
+            order: 7,
+            section: "Formatting",
+            type: "string"
+        },
+        midColor: {
+            display: "color",
+            display_size: "third",
+            label: "Middle",
+            order: 8,
+            section: "Formatting",
+            type: "string"
+        },
+        highColor: {
+            display: "color",
+            display_size: "third",
+            label: "High",
+            order: 9,
+            section: "Formatting",
+            type: "string"
+        },
+        applyTo: {
+            default: "all_numeric_fields",
+            display: "select",
+            label: "Apply to",
+            order: 10,
+            section: "Formatting",
+            type: "string",
+            values: [
+                {"All numeric fields": "all_numeric_fields"},
+                {"Select fields...": "select_fields"}
+            ]
+        },
+        // SERIES
+        showFullFieldName: {
+            default: true,
+            label: "Show Full Field Name",
+            order: 1,
+            section: "Series",
+            type: "boolean"
+        },
+        // PLOT
+        showSearchBar: {
+            default: false,
+            label: "Show Search Bar",
+            order: 1,
+            section: "Plot",
+            type: "boolean"
+        },
+        showPagination: {
+            default: false,
+            label: "Show Pagination",
+            order: 2,
+            section: "Plot",
+            type: "boolean"
+        },
+        showTableBorder: {
+            default: true,
+            label: "Show Table Border",
+            order: 3,
+            section: "Plot",
+            type: "boolean"
+        },
+        stripedRows: {
+            default: true,
+            label: "Striped Rows",
+            order: 4,
+            section: "Plot",
+            type: "boolean"
+        },
+        showRowNumbers: {
+            default: false,
+            label: "Show Row Numbers",
+            order: 5,
+            section: "Plot",
+            type: "boolean"
+        }
     },
-    perColumnRange: {
-      default: true,
-      hidden: true,
-      label: "Per column range",
-      order: 2,
-      section: "Formatting",
-      type: "boolean"
-    },
-    conditionalFormattingType: {
-      default: "all",
-      display: "select",
-      label: "Formatting Type",
-      order: 3,
-      section: "Formatting",
-      type: "string",
-      values: [
-        { All: "all" },
-        { "Subtotals only": "subtotals_only" },
-        { "Non-subtotals only": "non_subtotals_only" }
-      ]
-    },
-    includeNullValuesAsZero: {
-      default: false,
-      label: "Include Null Values as Zero",
-      order: 4,
-      section: "Formatting",
-      type: "boolean"
-    },
-    formattingStyle: {
-      default: "low_to_high",
-      display: "select",
-      label: "Format",
-      order: 5,
-      section: "Formatting",
-      type: "string",
-      values: [
-        { "From low to high": "low_to_high" },
-        { "From high to low": "high_to_low" }
-      ]
-    },
-    formattingPalette: {
-      default: "red_yellow_green",
-      display: "select",
-      label: "Palette",
-      order: 6,
-      section: "Formatting",
-      type: "string",
-      values: [
-        { "Red to Yellow to Green": "red_yellow_green" },
-        { "Red to White to Green": "red_white_green" },
-        { "Red to White": "red_white" },
-        { "White to Green": "white_green" },
-        { "Custom...": "custom" }
-      ]
-    },
-    lowColor: {
-      display: "color",
-      display_size: "third",
-      label: "Low", // These values updated in updateAsync
-      order: 7,
-      section: "Formatting",
-      type: "string"
-    },
-    midColor: {
-      display: "color",
-      display_size: "third",
-      label: "Middle",
-      order: 8,
-      section: "Formatting",
-      type: "string"
-    },
-    highColor: {
-      display: "color",
-      display_size: "third",
-      label: "High",
-      order: 9,
-      section: "Formatting",
-      type: "string"
-    },
-    applyTo: {
-      default: "all_numeric_fields",
-      display: "select",
-      label: "Apply to",
-      order: 10,
-      section: "Formatting",
-      type: "string",
-      values: [
-        { "All numeric fields": "all_numeric_fields" },
-        { "Select fields...": "select_fields" }
-      ]
-    },
-    // SERIES
-    showFullFieldName: {
-      default: true,
-      label: "Show Full Field Name",
-      order: 1,
-      section: "Series",
-      type: "boolean"
-    },
-    // PLOT
-    showSearchBar: {
-      default: false,
-      label: "Show Search Bar",
-      order: 1,
-      section: "Plot",
-      type: "boolean"
-    },
-    showPagination: {
-      default: false,
-      label: "Show Pagination",
-      order: 2,
-      section: "Plot",
-      type: "boolean"
-    },
-    showTableBorder: {
-      default: true,
-      label: "Show Table Border",
-      order: 1,
-      section: "Plot",
-      type: "boolean"
-    },
-    stripedRows: {
-      default: true,
-      label: "Striped Rows",
-      order: 2,
-      section: "Plot",
-      type: "boolean"
-    },
-    showRowNumbers: {
-      default: false,
-      label: "Show Row Numbers (wip)",
-      order: 3,
-      section: "Plot",
-      type: "boolean"
-    }
-  },
     /**
      * The create function gets called when the visualization is mounted but before any
      * data is passed to it.
      **/
-    create: function(element, config) {
+    create: function (element, config) {
         // Insert Bootstrap and DataTables css file
         element.innerHTML =
             '<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/css/bootstrap.css" />\
@@ -200,7 +200,7 @@ const customVizDataTable = {
      * UpdateAsync is the function that gets called (potentially) multiple times. It receives
      * the data and should update the visualization with the new data.
      **/
-    updateAsync: function(
+    updateAsync: function (
         data,
         element,
         config,
@@ -230,10 +230,12 @@ const customVizDataTable = {
         for (let i = 0; i < data.length; i++) {
             var row = data[i];
             var rowData = [];
+
             for (var key in row) {
                 rowData.push(row[key].value);
                 if (i == 0) {
                     columnCounter = 0;
+                    // ADD DIMENSIONS
                     for (var x = 0; x < queryResponse.fields.dimension_like.length; x++) {
                         if (queryResponse.fields.dimension_like[x]["name"] == key) {
                             var label = queryResponse.fields.dimension_like[x].label;
@@ -258,6 +260,7 @@ const customVizDataTable = {
                         }
                         columnCounter++;
                     }
+                    // ADD MEASURES
                     for (var x = 0; x < queryResponse.fields.measure_like.length; x++) {
                         if (queryResponse.fields.measure_like[x]["name"] == key) {
                             var label = queryResponse.fields.measure_like[x].label;
@@ -269,6 +272,28 @@ const customVizDataTable = {
                                     orderDirection = "desc";
                                 } else if (
                                     queryResponse.fields.measure_like[x].sorted.desc == false
+                                ) {
+                                    orderDirection = "asc";
+                                } else {
+                                    orderDirection = null;
+                                }
+                                sortArray.push([columnCounter, orderDirection]);
+                            }
+                        }
+                        columnCounter++;
+                    }
+                    // ADD TABLE CALCULATIONS
+                    for (var x = 0; x < queryResponse.fields.table_calculations.length; x++) {
+                        if (queryResponse.fields.table_calculations[x]["name"] == key) {
+                            var label = queryResponse.fields.table_calculations[x].label;
+                            var labelShort = queryResponse.fields.table_calculations[x].label_short;
+                            var type = queryResponse.fields.table_calculations[x].type;
+                            if (queryResponse.fields.table_calculations[x].sorted) {
+                                var orderDirection = "";
+                                if (queryResponse.fields.table_calculations[x].sorted.desc == true) {
+                                    orderDirection = "desc";
+                                } else if (
+                                    queryResponse.fields.table_calculations[x].sorted.desc == false
                                 ) {
                                     orderDirection = "asc";
                                 } else {
@@ -298,7 +323,7 @@ const customVizDataTable = {
                             render: $.fn.dataTable.render.number(",", ".", 0, "")
                         });
                     } else {
-                        headerArray.push({ title: columnTitle, type: type });
+                        headerArray.push({title: columnTitle, type: type});
                     }
                 }
             }
@@ -310,9 +335,9 @@ const customVizDataTable = {
         // Insert the generated html into the page
         this._vizContainer.innerHTML = html;
 
-        $(document).ready(function() {
+        $(document).ready(function () {
             //console.log(data);
-            $("#lookerDataTable").DataTable({
+            var table = $("#lookerDataTable").DataTable({
                 searching: config.showSearchBar,
                 paging: config.showPagination,
                 info: config.showPagination,
@@ -324,20 +349,32 @@ const customVizDataTable = {
                     footer: true
                 }
             });
+
+            // Show or hide row numbers
+            if (config.showRowNumbers == true) {
+                table.on('order.dt search.dt', function () {
+                    table.column(0, {search: 'applied', order: 'applied'}).nodes().each(function (cell, i) {
+                        cell.innerHTML = i + 1;
+                    });
+                }).draw();
+            } else {
+                table.
+            }
+
+            // Show or hide the table border
+            if (config.showTableBorder == true) {
+                $("#lookerDataTable").addClass("table-bordered");
+            } else {
+                $("#lookerDataTable").removeClass("table-bordered");
+            }
+            // Show or hide the table border
+            if (config.stripedRows == true) {
+                $("#lookerDataTable").addClass("table-striped");
+            } else {
+                $("#lookerDataTable").removeClass("table-striped");
+            }
         });
 
-        // Show or hide the table border
-        if (config.showTableBorder == true) {
-            $("#lookerDataTable").addClass("table-bordered");
-        } else {
-            $("#lookerDataTable").removeClass("table-bordered");
-        }
-        // Show or hide the table border
-        if (config.stripedRows == true) {
-            $("#lookerDataTable").addClass("table-striped");
-        } else {
-            $("#lookerDataTable").removeClass("table-striped");
-        }
 
         doneRendering();
     }
