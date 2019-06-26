88a3724681b226a1aea2fba17d5cd17cc42a2d8f
epschroeder <egbertschroeder@gmail.com>
2019-06-26 16:30:37 +0200

    Changed from hero to jumbotron

--- custom_viz_report_titlepage.js
+++ custom_viz_report_titlepage.js
@@ -13,105 +13,93 @@
  **/
 
 const customVizReportTitlePage = {
-    /**
-     * Configuration options
-     *
-     **/
+        /**
+         * Configuration options
+         *
+         **/
 
-    options: {
-        backgroundImageUrl: {
-            default: 'https://via.placeholder.com/1920x1080',
-            label: "Background Image URL",
-            order: 1,
-            type: "string"
+        options: {
+            backgroundImageUrl: {
+                default: 'https://via.placeholder.com/1920x1080',
+                label: "Background Image URL",
+                order: 1,
+                type: "string"
+            },
         },
-    },
 
-    /**
-     * The create function gets called when the visualization is mounted but before any
-     * data is passed to it.
-     **/
-    create: function (element, config) {
-        // Insert Bootstrap css file
-        element.innerHTML = `<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+        /**
+         * The create function gets called when the visualization is mounted but before any
+         * data is passed to it.
+         **/
+        create: function (element, config) {
+            // Insert Bootstrap css file
+            element.innerHTML = `<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
              <style>
                 body, html {
                     height: 100%;
                 }
 
-                /* The hero image */
-                .hero-image {
-                    /* Set a specific height */
-                    height: 100%;
-                    /* Position and center the image to scale nicely on all screens */
-                    background-position: center;
-                    background-repeat: no-repeat;
-                    background-size: cover;
-                    /*position: relative;*/
-                }   
-
-                /* Place text in the middle of the image */
-                .hero-text {
-                    text-align: center;
-                    position: absolute;
-                    top: 50%;
-                    left: 50%;
-                    transform: translate(-50%, -50%);
-                    color: #000000;
-                }
+                 .jumbotron {
+                color: white;
+                background-position: center;
+                background-repeat: no-repeat;
+                background-size: cover;
+                /*height: 100vh;*/
+            }
              </style>`;
 
-        // Create a container element to let us center the text.
-        this._vizContainer = element.appendChild(document.createElement("div"));
-        this._vizContainer.className = "container-fluid";
-    },
+            // Create a container element to let us center the text.
+            this._vizContainer = element.appendChild(document.createElement("div"));
+            this._vizContainer.className = "container-fluid";
+        },
 
-    /**
-     * UpdateAsync is the function that gets called (potentially) multiple times. It receives
-     * the data and should update the visualization with the new data.
-     **/
-    updateAsync: function (
-        data,
-        element,
-        config,
-        queryResponse,
-        details,
-        doneRendering
-    ) {
-        // Clear any errors from previous updates
-        this.clearErrors();
+        /**
+         * UpdateAsync is the function that gets called (potentially) multiple times. It receives
+         * the data and should update the visualization with the new data.
+         **/
+        updateAsync: function (
+            data,
+            element,
+            config,
+            queryResponse,
+            details,
+            doneRendering
+        ) {
+            // Clear any errors from previous updates
+            this.clearErrors();
 
-        // Throw some errors and exit if the shape of the data isn't what this chart needs
-        if (queryResponse.fields.dimensions.length < 2) {
-            this.addError({
-                title: "Not Enough Dimensions",
-                message: "This visualisation requires 2 dimensions."
-            });
-            return;
+            // Throw some errors and exit if the shape of the data isn't what this chart needs
+            if (queryResponse.fields.dimensions.length < 2) {
+                this.addError({
+                    title: "Not Enough Dimensions",
+                    message: "This visualisation requires 2 dimensions."
+                });
+                return;
+            }
+
+            // Select first row of data
+            var firstRow = data[0];
+
+            // Grab first column for the report title
+            var title = firstRow[queryResponse.fields.dimensions[0].name];
+
+            // Grab second column for the report subtitle/period
+            var subTitle = firstRow[queryResponse.fields.dimensions[1].name];
+
+            // Insert the data into text elements
+            var html = `<div class="jumbotron jumbotron-fluid" style="background-image: url('https://images.unsplash.com/photo-1438109491414-7198515b166b?q=80&fm=jpg&s=cbdabf7a79c087a0b060670a6d79726c')">
+  						<div class="container">
+    					<h1 class="display-4">title.value</h1>
+    					<p>This is a modified jumbotron that occupies the entire horizontal space of its parent.</p>
+  						</div>
+						</div>`;
+
+            // Insert the generated html into the page
+            this._vizContainer.innerHTML = html;
+            //console.log(config.backgroundImageUrl);
+            doneRendering();
         }
-
-        // Select first row of data
-        var firstRow = data[0];
-
-        // Grab first column for the report title
-        var title = firstRow[queryResponse.fields.dimensions[0].name];
-
-        // Grab second column for the report subtitle/period
-        var subTitle = firstRow[queryResponse.fields.dimensions[1].name];
-
-        // Insert the data into text elements
-        var html = `<div class="hero-image" style="background-image:url('` + config.backgroundImageUrl + `');">
-            <div class="hero-text">
-                <h1>` + title.value + `</h1>
-                <p>` + subTitle.value + `</p>
-            </div>
-        </div>`;
-
-        // Insert the generated html into the page
-        this._vizContainer.innerHTML = html;
-        //console.log(config.backgroundImageUrl);
-        doneRendering();
     }
-};
+;
 
 looker.plugins.visualizations.add(customVizReportTitlePage);
