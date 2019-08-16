a6a11794e70c53d75e8d55211e8e092fd92db557
epschroeder <egbertschroeder@gmail.com>
2019-08-16 11:17:25 +0200

    Added total row to table

--- custom_viz_datatable.js
+++ custom_viz_datatable.js
@@ -287,10 +287,10 @@ const customVizDataTable = {
                     if (queryResponse.totals_data[column]) {
                         // console.log('Column: ' + column);
                         // console.log(queryResponse.totals_data[column].html);
-                        totalArray.push(queryResponse.totals_data[column].html);
+                        totalArray.push('<strong>' + queryResponse.totals_data[column].html + '</strong>');
                     } else {
                         // console.log('Column: empty');
-                        totalArray.push('');
+                        totalArray.push('<strong>Total</strong>');
                     }
                 }
                 dataArray.push(totalArray);
