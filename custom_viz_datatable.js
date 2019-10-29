e5605cce8865e00d54738be3289aaafd30b1d972
epschroeder <egbertschroeder@gmail.com>
2019-10-29 16:15:38 +0100

    Added log for inserting rownumber

--- custom_viz_datatable.js
+++ custom_viz_datatable.js
@@ -332,13 +332,7 @@ const customVizDataTable = {
                     info: config.showPagination,
                     data: dataArray,
                     columns: headerArray,
-                    columnDefs: [{
-                        "searchable": false,
-                        "orderable": false,
-                        "targets": 0
-                    }],
-                    order: [[1, 'asc']],
-                    //order: sortArray,
+                    order: sortArray,
                     fixedHeader: {
                         header: true,
                         footer: true
@@ -375,16 +369,16 @@ const customVizDataTable = {
                 // Set table row font-size
                 $(".table tbody td").css({'font-size': config.rowFontSize + 'px'});
 
-                table.on('order.dt search.dt', function () {
-                    t.column(0, {search: 'applied', order: 'applied'}).nodes().each(function (cell, i) {
-                        cell.innerHTML = i + 1;
-                    });
-                }).draw();
+
             });
 
 
             // console.log(headerArray);
-            // console.log(totalArray);
+            //console.log(dataArray);
+
+            for (var i = 0; i < dataArray.length; i++) {
+                console.log(dataArray[i]);
+            }
             doneRendering();
         }
     }
