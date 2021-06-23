e60551d7980105c93ebf35a9ebf949f8904c177d
Egbert Schroeder <egbert.schroeder@persgroep.nl>
2021-06-23 11:07:42 +0200

    Removed classes to fix width when exporting to pdf

--- custom_viz_text.js
+++ custom_viz_text.js
@@ -5,94 +5,77 @@
  *
  * CREATED BY: Egbert Schroeder
  *
- * DEPENDENCIES:
- *
- *
- * TODO:
- *
- * [ ] Add Title Font Size Option
- * [ ] Add Paragraph Font Size Option
- * [ ] Add Limit Displayed Rows Options
- * [ ] Visualize All Records In Result Table
- *
  **/
 
 const customVizTextFromData = {
-    /**
-     * Configuration options
-     *
-     **/
+  /**
+   * Configuration options
+   *
+   **/
 
-    options: {},
+  options: {},
 
-    /**
-     * The create function gets called when the visualization is mounted but before any
-     * data is passed to it.
-     **/
-    create: function (element, config) {
-        // Insert Bootstrap css file
-        element.innerHTML =
-            '<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">';
+  /**
+   * The create function gets called when the visualization is mounted but before any
+   * data is passed to it.
+   **/
+  create: function (element, config) {
+    // Insert Bootstrap css file
+    element.innerHTML =
+      '<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet">';
 
-        // Create a container element to let us center the text.
-        this._vizContainer = element.appendChild(document.createElement("div"));
-        this._vizContainer.className = "container-fluid";
-    },
+    // Create a container element to let us center the text.
+    this._vizContainer = element.appendChild(document.createElement("div"));
+    this._vizContainer.className = "";
+  },
 
-    /**
-     * UpdateAsync is the function that gets called (potentially) multiple times. It receives
-     * the data and should update the visualization with the new data.
-     **/
-    updateAsync: function (
-        data,
-        element,
-        config,
-        queryResponse,
-        details,
-        doneRendering
-    ) {
-        // Clear any errors from previous updates
-        this.clearErrors();
+  /**
+   * UpdateAsync is the function that gets called (potentially) multiple times. It receives
+   * the data and should update the visualization with the new data.
+   **/
+  updateAsync: function (
+    data,
+    element,
+    config,
+    queryResponse,
+    details,
+    doneRendering
+  ) {
+    // Clear any errors from previous updates
+    this.clearErrors();
 
-        // Throw some errors and exit if the shape of the data isn't what this chart needs
-        if (queryResponse.fields.dimensions.length == 0) {
-            this.addError({
-                title: "No Dimensions",
-                message: "This chart requires dimensions."
-            });
-            return;
-        }
-
-        var html = '<div class="row">';
-        // Iterate through every row in the data
-        for (var x = 0; x < data.length; x++) {
-
-            var row = data[x];
-
-            // Grab first column for the title
-            var title = row[queryResponse.fields.dimensions[0].name];;
-
-            // Grab second column for the paragraph
-            var paragraph = row[queryResponse.fields.dimensions[1].name];
-
-             // Insert the data into text elements
-            html +=
-                '<h4 class="col-sm-12">' +
-                LookerCharts.Utils.htmlForCell(title) +
-                "</h4>";
-            html +=
-            '<p class="col-sm-12">' +
-            LookerCharts.Utils.textForCell(paragraph) +
-            "</p>";
-        }
-
-        html += "</div>";
-
-        // Insert the generated html into the page
-        this._vizContainer.innerHTML = html;
-
-        doneRendering();
+    // Throw some errors and exit if the shape of the data isn't what this chart needs
+    if (queryResponse.fields.dimensions.length == 0) {
+      this.addError({
+        title: "No Dimensions",
+        message: "This chart requires dimensions.",
+      });
+      return;
     }
+
+    var html = "";
+    // Iterate through every row in the data
+    for (var x = 0; x < data.length; x++) {
+      var row = data[x];
+
+      // Grab first column for the title
+      var title = row[queryResponse.fields.dimensions[0].name];
+
+      // Grab second column for the paragraph
+      var paragraph = row[queryResponse.fields.dimensions[1].name];
+
+      // Insert the data into text elements
+      html += "<h4>" + LookerCharts.Utils.htmlForCell(title) + "</h4>";
+      html += "<p>" + LookerCharts.Utils.textForCell(paragraph) + "</p>";
+    }
+
+    html += "";
+
+    // Insert the generated html into the page
+    this._vizContainer.innerHTML = html;
+
+    doneRendering();
+  },
 };
 
 looker.plugins.visualizations.add(customVizTextFromData);
